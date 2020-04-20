3efa3ec43f21a17fe64bde7f2734db10ed06209f
Jacob Ramsaywak <rams8940@mylaurier.ca>
2020-04-19 20:14:53 -0400

    connected to firebase -- use  xcworkspace to launch

--- iFridge/Login View Controllers/StartViewController.swift
+++ iFridge/Login View Controllers/StartViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import AVKit
+
 
 class StartViewController: UIViewController {
     
-
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var SignUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
@@ -19,12 +27,53 @@ class StartViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        setUpVideo()
+
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+    
+    
+    @IBAction func signInPressed(_ sender: Any) {
+    }
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+    }
+    
+    //Testing video login
+    func setUpVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "fridge_opening", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+    }
+
     
     @IBAction func unwindToStartView(segue:UIStoryboardSegue) {
         
